d830c1d3347a2758ac214de01b7a1058713dca77
dayanamh1998 <dayanamh1998@gmail.com>
2024-10-01 07:27:02 -0500

    Ejercicio numero 14 modificado y ensayado correctamente

--- EstadoCivil.ts
+++ EstadoCivil.ts
@@ -1,5 +1,15 @@
+
+
 export enum EstadoCivil {
     Soltero,
     Casado,
     Divorciado
 }
+
+
+// Ejemplo 
+const persona1 = new Persona('Juan', 30, EstadoCivil.Soltero);
+const persona2 = new Persona('María', 25, EstadoCivil.Casado);
+
+console.log(persona1.mostrarInfo());
+console.log(persona2.mostrarInfo());
