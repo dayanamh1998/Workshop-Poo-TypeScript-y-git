794ff1ae6a0b5a990dddaca8f8a29062a6457bbd
dayanamh1998 <dayanamh1998@gmail.com>
2024-09-30 21:07:00 -0500

    Ejercicio numero 13

--- util.ts
+++ util.ts
@@ -1,5 +1,7 @@
 import { Persona } from './Ejercicio2';
 
+
+
 export const crearPersonaDesdeJson = (objetoJson: any): Persona => {
     return new Persona(objetoJson.nombre, objetoJson.edad, objetoJson.direccion);
 };
@@ -8,4 +10,18 @@ export const crearYSaludar = () => {
     const personaJson = { nombre: "Juan", edad: 30, direccion: { calle: "Falsa", ciudad: "Springfield", pais: "USA" } };
     const persona = crearPersonaDesdeJson(personaJson);
     persona.saludar();
+};
+
+
+
+
+
+import empleadosData from './empleados.json';
+
+export const jsonAEmpleados = (data: any[]): Empleado[] => {
+    return data.map(emp => new Empleado(emp.nombre, emp.edad, emp.salario));
+};
+
+export const empleadosAJson = (empleados: Empleado[]): string => {
+    return JSON.stringify(empleados);
 };
